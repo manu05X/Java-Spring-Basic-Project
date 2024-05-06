8b0b5df72e53d734232057cd1c5a6cfd297dd6a7
Manish Kumar <k.manu00005@gmail.com>
2024-05-06 09:31:04 +0530

    First Commit

---
+++ src/main/java/com/example/productservicesproxy/ProductServicesProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.productservicesproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServicesProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServicesProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/productservicesproxy/ProductServicesProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productservicesproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServicesProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
