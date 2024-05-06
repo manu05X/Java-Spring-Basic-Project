4624bc12b38f5837303dfbf799db70beffa52fa6
Manish Kumar <k.manu00005@gmail.com>
2024-05-06 11:39:01 +0530

    adding the RequestMapping and RestController

--- src/main/java/com/example/productservicesproxy/controllers/ProductController.java
+++ src/main/java/com/example/productservicesproxy/controllers/ProductController.java
@@ -2,11 +2,23 @@ package com.example.productservicesproxy.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
     //Path
-    @GetMapping("/product/{id}")
+    @GetMapping("/{id}")
     public String getProductById(@PathVariable("id") Long id) {
         return "Product Fetched with id:"+id;
+    }
+    @GetMapping()
+    public List<String> getAllProducts() {
+        return Collections.emptyList();
     }
 }
