1e057a9e9b865bc50844e6241fb2812ddd737fc1
Manish Kumar <k.manu00005@gmail.com>
2024-05-10 23:08:12 +0530

    Adding AddProduct API

--- src/main/java/com/example/productservicesproxy/services/FakeStoreProductService.java
+++ src/main/java/com/example/productservicesproxy/services/FakeStoreProductService.java
@@ -5,7 +5,6 @@ import com.example.productservicesproxy.exceptions.ProductNotFoundException;
 import com.example.productservicesproxy.models.Categories;
 import com.example.productservicesproxy.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
